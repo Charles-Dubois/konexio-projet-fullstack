29a31c6dde339700102f9837733c14e5248f56bd
Charles-Dubois <charles.dubois.h@gmail.com>
2022-01-20 15:53:31 +0100

    commentaires

--- front/script.js
+++ front/script.js
@@ -1,21 +1,21 @@
 $(() => {
-    let latitude;
+    let latitude; // trois variable pour alimenter leaflet
     let longitude;
     let zoomLevel;
-    //                          affiche un spinner     placer en début de fonction appelé lors d'un click
+    //                       fonction pour afficher un spinner placer e
     function showMySpinner() {
         $(".countries-list").append(`<div class="spinner-border" role="status">
 <span class="visually-hidden">Loading...</span>
-</div>`)
+</div>`);
     }
-    //                        Supprime le spinner   placer en fin de fonction appelé lors d'un click
+    //                        Supprime le spinner
     function hideMySpinner() {
-        $(".spinner-border").remove()
+        $(".spinner-border").remove();
     }
-    //                  Affichage de tous les pays
+    //                  Affichage de tous les pays 
     function reset() {
-        $("li").remove()
-        showMySpinner()
+        $("li").remove(); //          Supprime les pays rcherché précedement
+        showMySpinner(); //           Appelle la fonction pour afficher un spinner
         $.ajax({
             url: "https://restcountries.com/v3.1/all",
             success: function(countries) {
@@ -24,21 +24,19 @@ $(() => {
                         `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital}
                      <br />Continent : ${country.region}</p></li>`
                     )
-                });
+                }); //              Importe tous les pays de l'API restcountries et les places dans les listes
             }
         }).then(() => {
-            hideMySpinner()
+            hideMySpinner(); //      Appelle la fonction qui supprime mon spinner
         })
     }
-    //                    Lance la fonction d'affichage à l'ouverture de la page
+    //                    Lance la fontion qui affiche tous les pays une fois en chargment de page
     reset()
-        //                     Boutton de réinitialisation de l'affichage
+        //                     Boutton de réinitialisation de l'affichage, on obtient la même chose qu'au moment d'arriver sur la page
     $("#btnReset").click(() => {
-        $("#map").remove()
-        reset()
+        $("#map").remove();
+        reset();
     })
-
-
 
 
     //                   Partie lefleat
@@ -60,7 +58,7 @@ $(() => {
         mainLayer.addTo(map);
     }
 
-
+    //                           Fin de partie leaflet
 
 
 
@@ -68,63 +66,54 @@ $(() => {
     //                     Déclaration de la fonction qui selectionne les pays
 
     function getAllCountries() {
-        $("#map").remove() // Réinitialise la map
-        $("li").remove() //   Réinitialise la liste
-        showMySpinner() // Spinner de chargement
-            //                       Défini le endpoint selon le choix du user : pays // capital // subregion
+        $("#map").remove(); // Efface une map précedement affichée
+        $("li").remove(); //   Supprime la liste précedment affiché
+        showMySpinner(); //    Affiche le spinner de chargement
+        //                       Défini le endpoint selon le choix du user : pays // capital // subregion
         let endPoint = "";
         if ($("#LookingForCountry").is(":checked")) {
-            endPoint = "https://restcountries.com/v3.1/name/"
+            endPoint = "https://restcountries.com/v3.1/name/";
         } else if ($("#LookingForCapital").is(":checked")) {
-            endPoint = "https://restcountries.com/v3.1/capital/"
+            endPoint = "https://restcountries.com/v3.1/capital/";
         } else if ($("#LookingForSubRegion").is(":checked")) {
-            endPoint = "https://restcountries.com/v3.1/subregion/"
+            endPoint = "https://restcountries.com/v3.1/subregion/";
         } else { console.log("il faut choisir") }
+
 
         $.ajax({
             // Importation de l'API rescountries
             url: endPoint + $("#countryForm").val(),
             success: function(countries) {
                 countries.forEach(country => {
-                    //                                 Retire tous les pays afficher pour affih=cher uniquement ceux recherchés
 
                     // Créer des élément de liste avec 3 clés nom du pays, capital, continent
                     $(".countries-list").append(
-                            `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital} <br />Continent : ${country.region}</p></li>`
-                        )
-                        // récupère les coordonées du pays
-                    latitude = country.latlng[0]
-                    longitude = country.latlng[1]
+                        `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital} <br />Continent : ${country.region}</p></li>`
+                    )
+
+                    // récupère les coordonées du pays
+                    latitude = country.latlng[0];
+                    longitude = country.latlng[1];
                 });
             },
         }).then(() => {
-            hideMySpinner()
+            hideMySpinner() //       Efface le spinner de chargement une fois l'interaction avec l'API terminée
         }).then(() => {
             if (endPoint === "https://restcountries.com/v3.1/name/" || "https://restcountries.com/v3.1/capital/") {
-                zoomLevel = 5;
+                zoomLevel = 5; //              Gros zoom si l'utilisateur cherche un pays
             } else if ("https://restcountries.com/v3.1/subregion/" === endPoint) {
-                zoomLevel = 1;
+                zoomLevel = 1; //             Petit zoom si l'utilisateur recherche une subregion
             }
-            $(".mapPosition").append(
-                `<div id="map"></div>`
+            $(".mapPosition").append( //          insère la map dans la page 
+                `<div id="map"></div>`;
             )
-            mapLoad()
+            mapLoad() //          charge la map
         })
-
 
     };
 
-
-
-
-
-
-    // appel ma fonction d'affichage du pays a chaque click
+    // appel ma fonction d'affichage du pays et de la map à chaque click ur show data
     $("#btnShowData").click(() => {
-        getAllCountries()
+        getAllCountries();
     })
-
-
-
-
 })
